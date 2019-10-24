532c071703326e0babe8cd804428ff784e9a6b6c
Adria Lopez <adria@prealfa.com>
2019-10-24 02:37:04 +0200

    Added help on sweethome3d. Fixed doc-script for disabling mate panel

--- disable-mate-panel.sh
+++ disable-mate-panel.sh
@@ -2,7 +2,7 @@
 
 # In order to disable the MATE panel we can remove it from the required components
 
-gsettings get org.mate.session.required-components panel
+gsettings set org.mate.session.required-components panel ""
 
 # Then kill it
 
